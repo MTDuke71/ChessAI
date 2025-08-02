a0713d1e9d01ea878067b071b2ca5f07bbfa4971
MTDuke71 <matt.laduke@gmail.com>
2025-08-02 19:56:20 -0400

    Added opeing book print to example.cpp

--- src/Example.cpp
+++ src/Example.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include "Board.h"
 #include "Engine.h"
 
@@ -7,5 +7,16 @@ int main() {
     Engine engine;
     std::string best = engine.searchBestMove(board, 2);
     std::cout << "Best move from starting position: " << best << "\n";
+    return 0;
+} */
+
+#include "OpeningBook.h"
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::string file = "books/komodo.bin";
+    if (argc > 1) file = argv[1];
+    OpeningBook book(file);
+    book.print(std::cout);
     return 0;
 }
